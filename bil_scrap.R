612ec7a778af1a8c58ad5cf4e716f52d7358bba8
Sebastian Tengborg <sebastian.tengborg@king.com>
2018-07-24 17:48:14 +0200

    graf per modell och ar

--- bil_scrap.R
+++ bil_scrap.R
@@ -4,6 +4,8 @@ library(readr)
 library(dplyr)
 library(ggplot2)
 library(plotly)
+library(RKing)
+options(scipen=999)
 
 i <- 2485 # Antal sidor
 snapshot_dt <- as.Date("2018-07-24")
@@ -12,7 +14,7 @@ for(i in 2:i) {
 if(i == 2) {
   start = Sys.time()
 }
-  print(paste0("Iteration: ", i))
+print(paste0("Iteration: ", i))
 url<- paste0('https://www.blocket.se/hela_sverige?q=&cg=1020&w=3&st=s&ps=&pe=&mys=&mye=&ms=&me=&cxpf=&cxpt=&fu=&gb=&ca=11&l=0&md=th&cp=&o=',i)
 webpage <- read_html(url)
 titel_html <- html_nodes(webpage, '.item_link')
@@ -36,8 +38,11 @@ if(i == 2) {
 
 if(i == max(i)) {
   slut = Sys.time()
+  tid = slut-start
+  print(tid)
   }
 }
+
 
 tot$snapshot_dt <- snapshot_dt
 
@@ -95,16 +100,45 @@ ggsave("pris_per_marke.png", width = 20, height = 20, units = "cm")
 
 
 # 2. Per modell
-# BÃ¶rjar med volvo
+# Börjar med volvo
 agg_modell <- c_tot %>% group_by(c_modell) %>% 
     filter(c_marke == "volvo") %>% 
     filter(ar %in% c("00","01","02","03","04","05","06","07","08","09","10",
-                     "11","12","13","14","15","16","17","18","19"))
+  "11","12","13","14","15","16","17","18","19"),
+  c_modell %in% c("v70","v60","xc60","v40","xc70","s60","v50","v90","xc90","s80"))
 
-p <- ggplot(agg_modell, aes(x=c_modell, y=kost, fill=c_modell)) + 
-    geom_boxplot() +
-    guides(fill=FALSE)
-ggplotly(p)
+df_agg_modell <-  agg_modell %>% group_by(c_modell) %>% summarise(n = n()) %>% arrange(desc(n))
+
+ggplot(agg_modell, aes(x=c_modell, y=kost, fill=c_modell)) + 
+  geom_boxplot() +
+  guides(fill=FALSE) +
+  labs(x = "", y = "Pris", title = "Pris för volvo modeller") +
+  theme(plot.title = element_text(hjust = 0.5), 
+    axis.line = element_blank())
+
 ggsave("pris_per_modell_volvo.png", width = 20, height = 20, units = "cm")
 
 
+
+# 3. Per modell och år
+agg_modell_ar <- c_tot %>% 
+    filter(c_marke == "volvo") %>% 
+    filter(ar %in% c("00","01","02","03","04","05","06","07","08","09","10",
+  "11","12","13","14","15","16","17","18","19"),
+  c_modell == "xc60") %>% 
+  group_by(ar) %>% 
+  summarise(avg_pris = mean(kost)) %>% 
+  ungroup %>% 
+  mutate(ar = as.factor(ar)) %>% 
+  mutate(position = factor(ar, levels = rev(levels(ar))))
+
+
+ggplot(agg_modell_ar, aes(x=position, y=avg_pris, group = 1)) + 
+  geom_line() +
+  geom_point() +
+  labs(x = "", y = "Genomsnittspris", title = "Pris för volvo XC60") +
+  theme(plot.title = element_text(hjust = 0.5), 
+    axis.line = element_blank(),
+    plot.background = element_blank())
+
+ggsave("pris_per_ar_xc60_volvo.png", width = 20, height = 20, units = "cm")
